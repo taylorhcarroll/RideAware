850293c6f44a2f3c6e6818d6da32ce3b117d711a
Taylor Carroll <taylorcarroll@Taylors-MacBook-Pro.local>
2019-11-04 13:05:13 -0600

    completed and incomplete lists now populating

--- src/components/dashboardViews/AdminDash.js
+++ src/components/dashboardViews/AdminDash.js
@@ -7,15 +7,9 @@ class AdminDash extends Component {
 		currentDate: '',
 		kidsArray: [],
 		availableKids: [],
-		completedKidsArray: [],
+		completedKids: [],
 		rides: []
 	};
-
-	// handleDelete = id => {
-	// 	EventsManager.delete(id).then(() => {
-	// 		this.props.getData();
-	// 	});
-	// };
 
 	componentDidMount() {
 		let newState = {}
@@ -24,7 +18,12 @@ class AdminDash extends Component {
 			newState.kidsArray = response
 			newState.availableKids = this.getIncompleteList(response, newState.curentDate)
 			// console.log("Admin Dash New State", newState)
-			this.getSomeKidsBruh(response, newState.currentDate)
+			newState.completedKids = this.getCompletedList(response, newState.currentDate)
+			this.setState({
+				currentDate: newState.currentDate,
+				availableKids: newState.availableKids,
+				completedKids: newState.completedKids
+			})
 		})
 	}
 
@@ -38,52 +37,40 @@ class AdminDash extends Component {
 			})
 	}
 	getIncompleteList = (kidsArray, currentDate) => {
-		return kidsArray.map(kid => {
-			console.log("kid", kid)
-			kid.rides.filter(ride => {
-				console.log("ride", ride)
-				let todayRide = false
-				console.log("ride date and current date", (ride.date === currentDate))
-				// ride.forEach(rideEvent => {
-				if (ride.date == currentDate) {
-					todayRide = true
-				}
-				//return todayRide
-				// });
-				return todayRide
-			})
-		})
-	}
-	getSomeKidsBruh = (kidsArray, currentDate) => {
-		console.log("getSomeKidsBruh KidsArray", kidsArray, "currentDate", currentDate)
+		//console.log("getSomeKidsBruh KidsArray", kidsArray, "currentDate", currentDate)
 		let availableKids = kidsArray.filter(kid => {
-			 return kid.rides.some(ride => {
-				 return ride.date !== currentDate
+			return kid.rides.some(ride => {
+				return ride.date !== currentDate
 			});
 		})
-		console.log("available Kids from getSomeKidsBruh", availableKids)
+		console.log("availableKids from getSomeKidsBruh", availableKids)
+		return availableKids
 	}
-	getCompletedList = () => {
-
-
+	getCompletedList = (kidsArray, currentDate) => {
+		//console.log("getSomeKidsBruh KidsArray", kidsArray, "currentDate", currentDate)
+		let completedKids = kidsArray.filter(kid => {
+			return kid.rides.some(ride => {
+				return ride.date === currentDate
+			});
+		})
+		console.log("completedKids from getSomeKidsBruh", completedKids)
+		return completedKids
 	}
 
 	getInProgressList = () => {
 
 	}
-	//  {this.state.cars_users.map(singleCarUser => {
-	// 	return singleCarUser.carId === this.props.cars_user.car.id ?
-	// 			<div key={singleCarUser.id}>
-	// 				<p>{singleCarUser.user.name}  </p>
 	render() {
 		return (
 			<>
 				<p>Hello this is the Admin Dashboard</p>
-				<p>This where kids who do not have a ride will go.</p>
+				<h5>This where kids who do not have a ride will go.</h5>
 				{this.state.availableKids.map(availableKid => (
 					<p>{availableKid.nickName}</p>))}
-				<p>This is where current rides will go, with a button to complete them.</p>
-				<p>This is where a list of kids who have been picked up will go.</p>
+				<h5>This is where current rides will go, with a button to complete them.</h5>
+				<h5>This is where a list of kids who have been picked up will go.</h5>
+				{this.state.completedKids.map(completedKid => (
+					<p>{completedKid.nickName}</p>))}
 			</>
 		);
 	}
